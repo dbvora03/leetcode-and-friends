6d2dad6268b9f9f4785a90a92cb3d96dd8196eba
dbvora03 <54694188+dbvora03@users.noreply.github.com>
2022-02-16 20:25:37 -0700

    692

---
+++ leetcode/python/692-top-k-frequent-words(M).py
@@ -0,0 +1,24 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        
+        counts = {}
+        for word in words:
+            if word in counts:
+                counts[word] += 1
+            else:
+                counts[word] = 1
+        
+        
+        import heapq
+        
+        heap = list(counts.items())
+        
+        heap = [(-j, i) for i, j in heap]
+        heapq.heapify(heap)
+
+        answer = []
+        for i in range(k):
+            value = heapq.heappop(heap)
+            answer.append(value[1])
+        
+        return answer
